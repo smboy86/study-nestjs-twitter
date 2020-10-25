d73616a68d9eff9fa109ece1a268f6b2b5b298ce
devpoi <smboy86@naver.com>
2020-10-26 02:27:56 +0900

    search users, create user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, ConflictException } from '@nestjs/common';
+import { User, UserCreateInput } from '@prisma/client';
+import { PrismaService } from '../prisma.service';
+import bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private db: PrismaService) {}
+
+  async findOne(username: string): Promise<User> {
+    const user = await this.db.user.findOne({
+      where: { username },
+    })
+
+    if(!user) {
+      throw new NotFoundException();
+    }
+
+    delete user.password;
+    return user;
+  }
+
+  async create(data: UserCreateInput): Promise<User> {
+    const existing = await this.db.user.findOne({
+      where: { username: data.username },
+    });
+
+    if(existing) {
+      throw new ConflictException('username_already_exists');
+    }
+
+    // const hashedPassword = bcrypt.hash(data.password, 10, function(err, hash): string{
+    //   console.log('result  ::: ', err, hash)
+    //   return "aaa";
+    // });
+    const hashedPassword = await bcrypt.hash(data.password, 10, function(err, hash): string{
+      console.log('result  ::: ', err, hash)
+      return "aaa";
+    });
+    // const hashedPassword = data.password;
+
+    const user = await this.db.user.create({
+      data: {
+        ...data,
+        password: hashedPassword,
+      },
+    });
+
+    console.log('create ::: ', user);
+
+    delete user.password;
+    return user;
+
+  }
+}
